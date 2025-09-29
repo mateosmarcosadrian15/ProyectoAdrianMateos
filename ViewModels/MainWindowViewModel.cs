5a630742820d9550b6abe04e12fb778e75c2dd2e
dam68 <dam68@erci.com>
2025-09-29 15:42:54 +0200

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿    using Avalonia.Controls;
+    using CommunityToolkit.Mvvm.ComponentModel;
+    using CommunityToolkit.Mvvm.Input;
+
+    namespace ProyectoAdrianMateos.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]public bool primerformulario = true;
+    [RelayCommand]
+    public void QuitarPrimerFormulario(object parameter)
+    {
+        CheckBox parametro = (CheckBox)parameter;
+        if (parametro.IsChecked == true)
+        {
+            primerformulario = false;
+        }
+        else
+        {
+            primerformulario = true;
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ProyectoAdrianMateos.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
