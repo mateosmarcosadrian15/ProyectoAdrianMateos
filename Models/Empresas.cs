6b0e203654adbf991949e0af88b15416244168c0
dam68 <dam68@erci.com>
2025-09-30 17:46:10 +0200

    update

---
+++ Models/Empresas.cs
@@ -0,0 +1,9 @@
+namespace ProyectoAdrianMateos.Models;
+
+public class Empresas
+{
+    public string nombreEmpresa { get; set; } =string.Empty;
+    public int numEmpleados { get; set; } = 0;
+    public string direccion {get;set;} = string.Empty;
+    public string telefono {get;set;} = string.Empty;
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
-﻿    using Avalonia.Controls;
+﻿    using System;
+    using System.Collections.ObjectModel;
+    using Avalonia.Controls;
     using CommunityToolkit.Mvvm.ComponentModel;
     using CommunityToolkit.Mvvm.Input;
+    using ProyectoAdrianMateos.Models;
 
     namespace ProyectoAdrianMateos.ViewModels;
 
@@ -8,17 +11,76 @@ public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
     [ObservableProperty]public bool primerformulario = true;
+    [ObservableProperty] public bool segundoformulario = true;
+    [ObservableProperty] public bool lista = false;
+    [ObservableProperty]public Empresas empresa = new();
+    [ObservableProperty] private ObservableCollection<Empresas> empresas = new();
+    
     [RelayCommand]
     public void QuitarPrimerFormulario(object parameter)
     {
         CheckBox parametro = (CheckBox)parameter;
-        if (parametro.IsChecked == true)
+        if (Primerformulario)
         {
-            primerformulario = false;
+            Primerformulario = false;
+            lista = true;
         }
         else
         {
-            primerformulario = true;
+            Primerformulario = true;
+            lista = false;
         }
+
+        if (parametro.IsChecked == true)
+        {
+            Segundoformulario = true;
+        }
+        else
+        {
+            Segundoformulario = false;
+        }
+    }
+
+    [RelayCommand]
+    public void QuitarSegundoformulario()
+    {
+        if (Segundoformulario)
+        {
+            Segundoformulario = true;
+        }
+        else
+        {
+            Segundoformulario = false;
+        }
+    }
+    [RelayCommand]
+    public void MostrarEmpresas()
+    {
+        if (string.IsNullOrWhiteSpace(Empresa.nombreEmpresa))
+        {
+            Console.Write("Ingrese el nombre de la empresa");
+        }
+        
+        else if (string.IsNullOrWhiteSpace(Empresa.direccion))
+        {
+            Console.Write("Ingrese la direccion de la empresa");
+        }
+        
+        else if (string.IsNullOrWhiteSpace(Empresa.telefono))
+        {
+            Console.Write("Ingrese telefono de la empresa");
+        }
+        
+        else if (Empresa.numEmpleados == 0)
+        {
+            Console.Write("Ingrese minimo 5 empleados");
+        }
+        else
+        {
+            Empresas.Add(empresa);
+            Empresa = new Empresas();
+            
+        }
+       
     }
 }
